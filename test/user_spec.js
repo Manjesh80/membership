d5dcf7ac8805877e84e47d8c501efd280c098127
Manjesh Gowda <manjesh.2j@gmail>
2014-09-15 19:14:28 -0400

    User tests

--- test/user_spec.js
+++ test/user_spec.js
@@ -3,6 +3,7 @@
  */
 var should = require("should");
 var User = require("../models/user");
+
 describe("User",function(){
     describe("defaults",function(){
 
@@ -14,13 +15,27 @@ describe("User",function(){
             });
         }); // before(function(){
 
-        it("email is manjesh.2j@gmail.com");
-        it("has an authentication token");
-        it("has a pending status ");
-        it("has a created date");
-        it("has a SignInCount of 0");
-        it("has a lastLogin");
-        it("has a currentLogin");
+        it("email is manjesh.2j@gmail.com", function(){
+            user.email.should.equal("manjesh.2j@gmail.com");
+        });
+        it("has an authentication token",function(){
+            user.authenticationToken.should.be.defined;
+        });
+        it("has a pending status ", function(){
+            user.status.should.equal("pending");
+        });
+        it("has a created date", function(){
+            user.createdAt.should.be.defined;
+        });
+        it("has a SignInCount of 0", function(){
+            user.signInCount.should.equal(0);
+        });
+        it("has a lastLogin", function(){
+            user.lastLoginAt.should.be.defined;
+        });
+        it("has a currentLogin", function(){
+            user.currentLoginAt.should.be.defined;
+        });
 
 
     }) ;// describe("defaults",function(){
